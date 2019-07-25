634f59889bc85c4a6e53689de9fd8dc3cd11db95
cn180450 <karthikreddy.kp@gmail.com>
2019-07-25 10:54:41 -0500

    image resizing

--- DataSetLoader.py
+++ DataSetLoader.py
@@ -5,33 +5,44 @@ import cv2
 from sklearn.preprocessing import LabelEncoder
 from keras.preprocessing.image import img_to_array
 from imutils import paths
+import imutils
 
 
 class DataSetLoader(object):
+    """
+    This class contains utilities to loads a dataset from a specified
+    path given that the inputs are in the path of below form
+        /path/to/data/{target_class_1}/{image_1}.jpg ... {image_n}.jpg
+        /path/to/data/{target_class_2}/{image_1}.jpg ... {image_n}.jpg
+        ...
+        ...
+        /path/to/data/{target_class_n}/{image_1}.jpg ... {image_n}.jpg
 
+    """
     def __init__(self, input_path, preprocessors=None):
         self.input_path = input_path
         self.preprocessors = preprocessors
 
     def get_data(self, **kwargs):
-        # input_path = self.input_path if input_path is None else input_path
+        """
+        Returns the data in the form of (images, labels) by parsing
+        an input directory
+        :param kwargs:
+        :return: Two numpy arrays containing images and target labels
+        """
         # input and target labels
         X = []
         y = []
-        images = list(paths.list_images(self.input_path)) \
+        images = list(imutils.paths.list_images(self.input_path)) \
             if kwargs["expand_path"] \
             else self.input_path
         for i, path in enumerate(images):
-            # valid given the input ->
-            # /path/to/data/
-            #   {target_class_1}/{image}.jpg
-            #   {target_class_2}/{image}.jpg
             _X = cv2.imread(path)
             _y = path.split(os.path.sep)[-2]
             # if any preprocessors
             if self.preprocessors:
                 for p in self.preprocessors:
-                    _X = p.preprocess(_X)
+                    _X = p._preprocess(_X)
             X.append(_X)
             y.append(_y)
         y = np.array(y)
@@ -50,8 +61,61 @@ class BasePreprocessor(object):
     def __init__(self, data_format="channels_last"):
         self.data_format = data_format
 
-    def preprocess(self, image):
+    def _preprocess(self, image):
         return img_to_array(image, data_format=self.data_format)
+
+
+class RescalePreprocessor(object):
+    """
+    This class scales image to the given width and height, also
+    one can choose to keep the aspect ratio intact after resizing
+    """
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def _preprocess(self, image, **kwargs):
+        """
+        Resizes the given image according to new dimensions, one can
+        choose to keep or ignore aspect ratio of original image by
+        passing the aspect_aware flag
+
+        :param image: The input image
+        :param kwargs:
+        :return: resized image according to new dimensions
+        """
+        # if interpolation is specified then use it or else use INTER_AREA
+        interpolation = kwargs["interpolation"] if kwargs["interpolation"] \
+            else cv2.INTER_AREA
+        # if specified to preserve aspect ratio
+        if kwargs["aspect_aware"]:
+            h, w = image.shape[:2]
+            dW = 0
+            dH = 0
+            # if width is smaller than height
+            if w < h:
+                image = imutils.resize(
+                    image,
+                    width=self.width,
+                    inter=interpolation
+                )
+                dH = int((image.shape[0] - self.height) / 2.0)
+            # if height is smaller than width
+            else:
+                image = imutils.resize(
+                    image,
+                    height=self.height,
+                    inter=interpolation
+                )
+                dW = int((image.shape[1] - self.width) / 2.0)
+            h, w = image.shape[:2]
+            image = image[dH:h - dH, dW:w - dW]
+        # finally return the resized image
+        return cv2.resize(
+            image,
+            (self.width, self.height),
+            interpolation=interpolation
+        )
 
 
 def _bgr_to_rgb(self, images):
