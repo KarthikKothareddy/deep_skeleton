1f84cbdae545d928beb2b67313751849bccc88cd
cn180450 <karthikreddy.kp@gmail.com>
2019-07-25 08:13:09 -0500

    refactor dataset loader

--- DataSetLoader.py
+++ DataSetLoader.py
@@ -2,6 +2,8 @@
 import os
 import numpy as np
 import cv2
+from sklearn.preprocessing import LabelEncoder
+from imutils import paths
 
 
 class DataSetLoader(object):
@@ -9,15 +11,25 @@ class DataSetLoader(object):
     def __init__(self, input_path):
         self.input_path = input_path
 
-    def get_data(self):
+    def get_data(self, **kwargs):
         # input_path = self.input_path if input_path is None else input_path
         # input and target labels
         X = []
         y = []
-        for i, path in enumerate(self.input_path):
-            # valid given the input -> /path/to/data/{class}/{image}.jpg
+        images = list(paths.list_images(self.input_path)) \
+            if kwargs["expand_path"] \
+            else self.input_path
+        for i, path in enumerate(images):
+            # valid given the input ->
+            # /path/to/data/
+            #   {target_class_1}/{image}.jpg
+            #   {target_class_2}/{image}.jpg
             _X = cv2.imread(path)
             _y = path.split(os.path.sep)[-2]
             X.append(_X)
             y.append(_y)
-        return np.array(X), np.array(y)
+        if kwargs["encode_labels"]:
+            encoder = LabelEncoder()
+            y = encoder.fit_transform(np.array(y))
+        return np.array(X), y
+
